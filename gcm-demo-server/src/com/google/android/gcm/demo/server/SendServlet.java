a79c511006e9e4aae3fc63a62a2befca349eb871
unknown <roman.tokmakov@HRK1-DHP-F30935.synapse.com>
2014-02-27 18:49:54 +0200

    Initial commit

---
+++ gcm-demo-server/src/com/google/android/gcm/demo/server/SendServlet.java
@@ -0,0 +1,38 @@
+package com.google.android.gcm.demo.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@SuppressWarnings("serial")
+public class SendServlet extends BaseServlet {
+	//private Sender sender;
+
+	  
+	  public void initF(ServletConfig config) throws ServletException {
+	  }
+	  
+	  
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+
+		out.print("fle3gfx3fy3v1yutxr12yuxbtr12yubtr");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, ServletException {
+		doPost(req, resp);
+	}
+
+}
